14c9a119246e6ca3925a01b7a048096924c824e8
Inara Oliveira <inaraolliveira23@hotmail.com>
2021-10-19 22:29:01 -0300

    Envia atividade de componente de classe

---
+++ src/ComponenteClasse.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ComponenteClasse extends React.Component{
+    render() {
+        return (
+            <div className='container'>
+                <h1>Olá mundo React JS</h1>
+                <h2>Esse é meu componente de classe ☆ ~ (‘▽ ^ 人)</h2>
+            </div>
+        );
+    };
+};
